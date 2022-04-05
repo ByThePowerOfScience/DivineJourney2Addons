a5908c76a62f44d9cbb18aecd388c00d3253a01d
ByThePowerOfScience <public.bythepowerofscience@gmail.com>
2022-04-04 21:40:56 -0400

    Made IF Plant Interactor take drops directly from AgriCraft crops.

--- src/main/java/org/btpos/dj2addons/DJ2Addons.java
+++ src/main/java/org/btpos/dj2addons/DJ2Addons.java
@@ -9,23 +9,26 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(
-		modid = Dj2addons.MOD_ID,
-		name = Dj2addons.MOD_NAME,
-		version = Dj2addons.VERSION
+		modid = DJ2Addons.MOD_ID,
+		name = DJ2Addons.MOD_NAME,
+		version = DJ2Addons.VERSION
 )
-public class Dj2addons {
+public class DJ2Addons {
 	
 	public static final String MOD_ID = "dj2addons";
-	public static final String MOD_NAME = "Dj2addons";
-	public static final String VERSION = "1.0-SNAPSHOT";
+	public static final String MOD_NAME = "Divine Journey 2 Addons";
+	public static final String VERSION = "1.0.1";
+	public static final Logger LOGGER = LogManager.getLogger("DJ2Addons");
 	
 	/**
 	 * This is the instance of your mod as created by Forge. It will never be null.
 	 */
 	@Mod.Instance(MOD_ID)
-	public static Dj2addons INSTANCE;
+	public static DJ2Addons INSTANCE;
 	
 	/**
 	 * This is the first initialization event. Register tile entities here.
---
+++ src/main/java/org/btpos/dj2addons/mixin/industrialforegoing/MPlantInteractor.java
@@ -0,0 +1,42 @@
+package org.btpos.dj2addons.mixin.industrialforegoing;
+
+import com.buuz135.industrial.tile.agriculture.PlantInteractorTile;
+import com.infinityraider.agricraft.api.v1.misc.IAgriHarvestable;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.items.ItemHandlerHelper;
+import net.minecraftforge.items.ItemStackHandler;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.List;
+
+@Mixin(PlantInteractorTile.class)
+public class MPlantInteractor {
+	@Shadow
+	private ItemStackHandler outItems;
+	
+	@Shadow @Final
+	public static List WORKING_TILES;
+	
+	private static final IBlockState DUMMY = Blocks.AIR.getDefaultState();
+	
+	@Redirect(method="work()F",  at=@At(value="INVOKE", target="Lnet/minecraft/world/World;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/state/IBlockState;"))
+	private IBlockState handleAgriCraftCrops(World world, BlockPos pos) {
+		TileEntity te = world.getTileEntity(pos);
+		if (te instanceof IAgriHarvestable) {
+			WORKING_TILES.add(this);
+			((IAgriHarvestable)te).onHarvest(stack -> ItemHandlerHelper.insertItem(outItems, stack, false), null);
+			WORKING_TILES.remove(this);
+			return DUMMY;
+		} else {
+			return world.getBlockState(pos);
+		}
+	}
+}
