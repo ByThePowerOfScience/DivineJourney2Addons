95ba038235a23d5f644ce4dfbd0af5dac791b1eb
ByThePowerOfScience <public.bythepowerofscience@gmail.com>
2022-08-30 00:36:15 -0400

    Fixes Divine-Journey-2|Divine-Journey-2#668. (Untested)

---
+++ src/main/java/org/btpos/dj2addons/mixin/def/tweaks/modularmagic/MRequirementLifeEssence.java
@@ -0,0 +1,30 @@
+package org.btpos.dj2addons.mixin.def.tweaks.modularmagic;
+
+import fr.frinn.modularmagic.common.crafting.requirement.RequirementLifeEssence;
+import fr.frinn.modularmagic.common.tile.TileLifeEssenceProvider;
+import hellfirepvp.modularmachinery.common.crafting.helper.RecipeCraftingContext;
+import hellfirepvp.modularmachinery.common.machine.MachineComponent;
+import hellfirepvp.modularmachinery.common.util.ResultChance;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(RequirementLifeEssence.class)
+public abstract class MRequirementLifeEssence {
+	@Inject(
+			remap = false,
+			method = "finishCrafting",
+			at = @At(
+					target = "Lfr/frinn/modularmagic/common/tile/TileLifeEssenceProvider;getSoulNetwork()LWayofTime/bloodmagic/core/data/SoulNetwork;",
+					value = "INVOKE",
+					shift = At.Shift.BEFORE
+			),
+			cancellable = true
+	)
+	private void checkNull(MachineComponent component, RecipeCraftingContext context, ResultChance chance, CallbackInfoReturnable<Boolean> cir) {
+		if (component.getContainerProvider() == null || ((TileLifeEssenceProvider) component.getContainerProvider()).getSoulNetwork() == null)
+			cir.setReturnValue(true);
+	}
+}
+
