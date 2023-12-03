477e7d279ddebd987f2ea9b14af41c1c0116571e
ByThePowerOfScience <public.bythepowerofscience@gmail.com>
2023-12-02 22:38:37 -0500

    Fixed NPE

--- mod/src/main/java/btpos/dj2addons/crafttweaker/extrautils2/CTBiomeMarker.java
+++ mod/src/main/java/btpos/dj2addons/crafttweaker/extrautils2/CTBiomeMarker.java
@@ -1,14 +1,16 @@
 package btpos.dj2addons.crafttweaker.extrautils2;
 
+import btpos.dj2addons.api.extrautils2.ExtraUtilities;
+import btpos.dj2addons.common.util.zendoc.ZenDocArg;
+import btpos.dj2addons.common.util.zendoc.ZenDocClass;
+import btpos.dj2addons.common.util.zendoc.ZenDocMethod;
+import crafttweaker.CraftTweakerAPI;
 import crafttweaker.annotations.ModOnly;
 import crafttweaker.annotations.ZenDoc;
 import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.minecraft.CraftTweakerMC;
 import crafttweaker.api.world.IBiome;
-import btpos.dj2addons.common.util.zendoc.ZenDocArg;
-import btpos.dj2addons.common.util.zendoc.ZenDocClass;
-import btpos.dj2addons.common.util.zendoc.ZenDocMethod;
-import btpos.dj2addons.api.extrautils2.ExtraUtilities;
+import net.minecraft.world.biome.Biome;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
@@ -22,6 +24,11 @@ public class CTBiomeMarker {
 			@ZenDocArg(value = "biome", info = "The biome to exclude.")
 	}) @ZenDoc("Excludes biome from Biome Marker. See docs on GitHub.")
 	public static void excludeBiome(IBiome biome) {
-		ExtraUtilities.addExcludedBiome(CraftTweakerMC.getBiome(biome).getRegistryName());
+		Biome biome1 = CraftTweakerMC.getBiome(biome);
+		if (biome1 == null) {
+			CraftTweakerAPI.logError("[dj2addons.extrautils2.BiomeMarker#excludeBiome] Unable to find biome for \"" + biome + "\".");
+			return;
+		}
+		ExtraUtilities.addExcludedBiome(biome1.getRegistryName());
 	}
 }
