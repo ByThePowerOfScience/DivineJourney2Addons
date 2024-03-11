cb307d3688bc52c9e35559e96d01bb5c64412b47
ByThePowerOfScience <public.bythepowerofscience@gmail.com>
2024-03-11 12:43:33 -0400

    Refactored to conform with Mixin spec. Also used Injects instead of an Overwrite to ensure it links to the inherited isDummy method.

--- mod/src/main/java/btpos/dj2addons/patches/mixin/immersiveengineering/MTileEntityMultiblockPart.java
+++ mod/src/main/java/btpos/dj2addons/patches/mixin/immersiveengineering/MTileEntityMultiblockPart.java
@@ -1,32 +1,26 @@
 package btpos.dj2addons.patches.mixin.immersiveengineering;
 
+import blusunrize.immersiveengineering.common.blocks.TileEntityMultiblockPart;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import blusunrize.immersiveengineering.common.blocks.TileEntityMultiblockPart;
-import blusunrize.immersiveengineering.common.blocks.TileEntityIEBase;
-import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.IBlockBounds;
-import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.IDirectionalTile;
-import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.IGeneralMultiblock;
-import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces.ITileDrop;
-import net.minecraft.util.ITickable;
 
-@Mixin(TileEntityMultiblockPart.class)
-public abstract class MTileEntityMultiblockPart extends TileEntityIEBase
-        implements ITickable, IDirectionalTile, IBlockBounds, IGeneralMultiblock
-{
+@Mixin(value=TileEntityMultiblockPart.class, remap=false)
+public abstract class MTileEntityMultiblockPart {
     @Shadow
     public int[] offset;
-
-    //Original is like this:
-    //public boolean isDummy() {
-    //    return this.offset[0] != 0 || this.offset[1] != 0 || this.offset[2] != 0;
-    //}
-    public boolean isDummy() {
-        return (this.offset.length >= 1 && this.offset[0] != 0) ||
+    
+    @Inject(
+        method="isDummy",
+        at=@At("HEAD"),
+        cancellable = true
+    )
+    public void softOverwrite_fixInvalidOffsetSize(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(
+                (this.offset.length >= 1 && this.offset[0] != 0) ||
                 (this.offset.length >= 2 && this.offset[1] != 0) ||
-                (this.offset.length >= 3 && this.offset[2] != 0);
+                (this.offset.length >= 3 && this.offset[2] != 0));
     }
 }
