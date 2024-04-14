076e36db99c82aa3bc4552467f13d74382de0cf2
ByThePowerOfScience <public.bythepowerofscience@gmail.com>
2024-04-14 06:24:36 -0400

    fixed coremod inject

--- python/ModOutput_InjectCoreMod.py
+++ python/ModOutput_InjectCoreMod.py
@@ -1,24 +1,34 @@
 import glob
-import os
+import re
 import zipfile
 from pathlib import Path
-from packaging import version
-import re
 
-# x = re.compile("/dj2addons-([0-9.]+)\\.jar/")
+from packaging import version
+
+versionRegex = re.compile(r"dj2addons-([0-9.]+(?:-\w+)?)\.jar")
 
 
 def getModPath(s):
-    possibles = glob.glob(s)
-    if len(possibles) == 0:
-        raise RuntimeError("No possible mod jars found at " + s)
-    # latest = version.parse("0.0.0")
-    # latestjar = None
-    for x in possibles:
-        if 'sources' in x:
-            continue
-        return x
-    raise RuntimeError('No mod jar found at ' + str([path for [path, _jar] in possibles]))
+	possibles = glob.glob(s)
+	if len(possibles) == 0:
+		raise RuntimeError("No possible mod jars found at " + s)
+	
+	latestVersion = version.parse("0.0.0")
+	latestJarPath = None
+	
+	for jarPath in possibles:
+		jarName = jarPath.split("/")[-1]
+		if 'sources' in jarName:
+			continue
+		re_jarVersionMatch = versionRegex.match(jarName)
+		if re_jarVersionMatch is not None:
+			jarVersion = re_jarVersionMatch.groups()[0]
+			if (version.parse(jarVersion) > latestVersion):
+				latestJarPath = jarPath
+	
+	if latestJarPath is not None:
+		return latestJarPath
+	raise RuntimeError('No mod jar found at ' + str([possibles]))
 
 
 project_dir = Path("~/IdeaProjects/DJ2Addons/").expanduser()
@@ -27,9 +37,9 @@ mod_path = project_dir / "mod/build/libs/dj2addons-*.jar"
 
 
 def main():
-    modjar = getModPath(str(mod_path))
-    with zipfile.ZipFile(modjar, 'a') as file:
-        file.write(coremod_path, '/META-INF/libraries/dj2addons-core.jar')
+	modjar = getModPath(str(mod_path))
+	with zipfile.ZipFile(modjar, mode='a') as file:
+		file.write(coremod_path, '/META-INF/libraries/dj2addons-core.jar')
 
 
 main()
