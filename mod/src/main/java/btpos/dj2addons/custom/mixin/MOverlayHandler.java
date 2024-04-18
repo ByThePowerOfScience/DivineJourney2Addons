80dc514163e2b93abf326203f09c8b1a325164b0
ByThePowerOfScience <public.bythepowerofscience@gmail.com>
2024-04-18 19:20:18 -0400

    MOverlayHandler: Reverted MixinExtras usage for MixinBooter 7.0

--- mod/src/main/java/btpos/dj2addons/custom/mixin/MOverlayHandler.java
+++ mod/src/main/java/btpos/dj2addons/custom/mixin/MOverlayHandler.java
@@ -1,15 +1,14 @@
 package btpos.dj2addons.custom.mixin;
 
-import btpos.dj2addons.custom.impl.StatusEffects;
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.Share;
-import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
+import btpos.dj2addons.custom.registry.ModPotions.Registered;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiIngame;
 import net.minecraft.entity.player.EntityPlayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import squeek.appleskin.client.HUDOverlayHandler;
@@ -21,21 +20,47 @@ import static org.spongepowered.asm.mixin.injection.At.Shift.AFTER;
  */
 @Mixin(HUDOverlayHandler.class)
 abstract class MOverlayHandler {
-	@Inject(method="drawSaturationOverlay(FFLnet/minecraft/client/Minecraft;IIF)V", at=@At(target="Lnet/minecraft/client/renderer/texture/TextureManager;bindTexture(Lnet/minecraft/util/ResourceLocation;)V", value="INVOKE", shift=AFTER))
-	private static void getRegenValue(float saturationGained, float saturationLevel, Minecraft mc, int left, int top, float alpha, CallbackInfo ci, @Share("dj2addons$regen") LocalIntRef regen) {
-		EntityPlayer player = (EntityPlayer)mc.getRenderViewEntity();
-		if (player == null || !player.isPotionActive(StatusEffects.UIEffectTrigger_HungerShankWave.apply(null))) {
-			regen.set(-1);
+	
+	@Unique private static int dj2addons$i = -1;
+	@Unique private static int dj2addons$regen = -1;
+	
+	@Inject(
+			method = "drawSaturationOverlay(FFLnet/minecraft/client/Minecraft;IIF)V",
+			at = @At(
+					target = "Lnet/minecraft/client/renderer/texture/TextureManager;bindTexture(Lnet/minecraft/util/ResourceLocation;)V",
+					value = "INVOKE",
+					shift = AFTER
+			)
+	)
+	private static void getRegenValue(float saturationGained, float saturationLevel, Minecraft mc, int left, int top, float alpha, CallbackInfo ci) {
+		EntityPlayer player = (EntityPlayer) mc.getRenderViewEntity();
+		if (player == null || player.isPotionActive(Registered.saturegen)) {
+			dj2addons$regen = -1;
 		} else {
-			regen.set(mc.ingameGUI.getUpdateCounter() % 25);
+			dj2addons$regen = mc.ingameGUI.getUpdateCounter() % 25;
 		}
 	}
 	
-	@Redirect(method= "drawSaturationOverlay(FFLnet/minecraft/client/Minecraft;IIF)V", at=@At(target="Lnet/minecraft/client/gui/GuiIngame;drawTexturedModalRect(IIIIII)V", value="INVOKE"))
-	private static void saturationOverlayRegenEffect(GuiIngame instance, int x, int y, int textureX, int textureY, int width, int height,
-	                                                 @Local(name="i") int i, @Share("dj2addons$regen") LocalIntRef regen)
-	{
-		if (i == regen.get()) {
+	@ModifyVariable(
+			remap = false,
+			method = "drawSaturationOverlay(FFLnet/minecraft/client/Minecraft;IIF)V",
+			name = "i",
+			at = @At("LOAD")
+	)
+	private static int getIteratorVal(int value) {
+		dj2addons$i = value;
+		return value;
+	}
+	
+	@Redirect(
+			method = "drawSaturationOverlay(FFLnet/minecraft/client/Minecraft;IIF)V",
+			at = @At(
+					target = "Lnet/minecraft/client/gui/GuiIngame;drawTexturedModalRect(IIIIII)V",
+					value = "INVOKE"
+			)
+	)
+	private static void saturationOverlayRegenEffect(GuiIngame instance, int x, int y, int textureX, int textureY, int width, int height) {
+		if (dj2addons$i == dj2addons$regen) {
 			y -= 2;
 		}
 		instance.drawTexturedModalRect(x, y, textureX, textureY, width, height);
